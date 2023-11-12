d943c38ed9aa2051a0a5eed1417d995d7d898686
DuyDangCode <21520774@gm.uit.edu.vn>
2023-11-12 17:37:50 +0700

    basic navbar

---
+++ components/nav/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Container from "../Container";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({
+  weight: "400",
+  style: "normal",
+  subsets: ["latin"],
+});
+
+const NavBar = () => {
+  return (
+    <div className="sticky w-full z-30 bg-slate-200 top-0 shadow-sm">
+      <Container>
+        <div className="flex w-full justify-between items-center gap-3 md:gap-0">
+          <Link
+            href={"/"}
+            className={`${redressed.className} font-bold text-2xl`}
+          >
+            E-Shop
+          </Link>
+          <div className="hidden sm:block">Search</div>
+          <div className="flex gap-8 md:gap-12 justify-between">
+            <div className="">Card</div>
+            <div className="">Avatar</div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default NavBar;
