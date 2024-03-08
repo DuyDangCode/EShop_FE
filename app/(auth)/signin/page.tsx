9900c238529d8fb8aa08346d57cae8d8318f0a24
DuyDangCode <21520774@gm.uit.edu.vn>
2024-03-08 21:51:37 +0700

    #7 add react query

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,10 +1,42 @@
-import SigninForm from '@/components/SigninForm'
+'use client'
+import { pathHelper } from '@/helper/router'
+import Link from 'next/link'
 
 export default function SignIn() {
   return (
-    <div>
-      this sign in
-      <SigninForm />
+    <div className='w-full h-full flex justify-center items-center'>
+      <form className=' flex flex-col w-[20rem] h-[30rem] gap-2 justify-center'>
+        <label htmlFor='username' className=' font-bold text-[2rem]'>
+          Username
+        </label>
+        <input
+          type='text'
+          name='username'
+          id='username'
+          required
+          className=' h-10 rounded-sm p-2'
+        />
+        <label htmlFor='password' className='font-bold  text-[2rem] mt-5'>
+          Password
+        </label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          required
+          className=' h-10 rounded-sm p-2'
+        />
+
+        <button
+          type='submit'
+          className=' mt-7 border-black border-2 h-10 rounded-md'
+        >
+          Signin
+        </button>
+        <Link href={pathHelper.signup()} className=' text-black'>
+          {"Don't have an account?"}
+        </Link>
+      </form>
     </div>
   )
 }
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import UserContext, { User } from '@/context/userContext'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode, useEffect, useState } from 'react'
 import { Toaster } from 'react-hot-toast'
+import { queryClient } from './queryClient'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export function Provider({
   children,
@@ -16,9 +19,12 @@ export function Provider({
     setUser(defaultUser)
   }, [defaultUser])
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
-      <Toaster position='bottom-center' reverseOrder={false} />
-    </UserContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <UserContext.Provider value={{ user, setUser }}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={true} />
+        <Toaster position='bottom-center' reverseOrder={false} />
+      </UserContext.Provider>
+    </QueryClientProvider>
   )
 }
