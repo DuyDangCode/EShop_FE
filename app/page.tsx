a635a2c1a1bfceeebbe7cbe0f1eed119c4cc9779
Thành Duy <21520774@gm.uit.edu.vn>
2024-03-27 09:56:46 +0700

    #13 add carousel package

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,15 @@
+'use client'
 import ProductCard from '@/components/ProductCard'
 import { pathHelper } from '@/helper/router'
 import Link from 'next/link'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-export default async function Home() {
+export default function Home() {
   return (
     <div>
-      <div className=' flex'>
-        <ProductCard
-          name={
-            'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On the store aaaaaaaaaaaaaaa'
-          }
-          price={500}
-          priceBeforeDiscount={1000}
-          srcImage={'asas'}
-          ratingScore={3}
-          reviewAmount={300}
-          quantity={0}
-        />
+      <Carousel width={'1200px'}></Carousel>
+      <div className='flex w-fit'>
         <ProductCard
           name={
             'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On the store aaaaaaaaaaaaaaa'
@@ -73,8 +66,63 @@ export default async function Home() {
           quantity={0}
         />
       </div>
-
-      <Link href={pathHelper.signin()}>Sign in page</Link>
+      <div className='flex w-full'>
+        <ProductCard
+          name={
+            'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On the store aaaaaaaaaaaaaaa'
+          }
+          price={500}
+          priceBeforeDiscount={1000}
+          srcImage={'asas'}
+          ratingScore={3}
+          reviewAmount={300}
+          quantity={0}
+        />
+        <ProductCard
+          name={
+            'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On the store aaaaaaaaaaaaaaa'
+          }
+          price={500}
+          priceBeforeDiscount={1000}
+          srcImage={'asas'}
+          ratingScore={3}
+          reviewAmount={300}
+          quantity={0}
+        />
+        <ProductCard
+          name={
+            'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On the store aaaaaaaaaaaaaaa'
+          }
+          price={500}
+          priceBeforeDiscount={1000}
+          srcImage={'asas'}
+          ratingScore={3}
+          reviewAmount={300}
+          quantity={0}
+        />
+        <ProductCard
+          name={
+            'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On the store aaaaaaaaaaaaaaa'
+          }
+          price={500}
+          priceBeforeDiscount={1000}
+          srcImage={'asas'}
+          ratingScore={3}
+          reviewAmount={300}
+          quantity={0}
+        />
+        <ProductCard
+          name={
+            'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On the store aaaaaaaaaaaaaaa'
+          }
+          price={500}
+          priceBeforeDiscount={1000}
+          srcImage={'asas'}
+          ratingScore={3}
+          reviewAmount={300}
+          quantity={0}
+        />
+      </div>
     </div>
   )
 }
